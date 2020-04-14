2da4216ce611efb3ed15bc2fe09ea1da45b506ed
Jingyu Shi <julieshi@usc.edu>
2020-04-14 15:40:58 +0800

    react-flask-framework

---
+++ flask-backend/static/react/precache-manifest.7dfc8bcca2eb9b92573af21d5cd6197e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4812a61c2ea2a156c578f5f9e0d34bef",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "367659934f7680c31ef8",
+    "url": "/static/react/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ec829bd0073e9a4f802c",
+    "url": "/static/react/js/2.916cb700.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.916cb700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "367659934f7680c31ef8",
+    "url": "/static/react/js/main.a37fa2d1.chunk.js"
+  },
+  {
+    "revision": "c7241993fd2c7bed7213",
+    "url": "/static/react/js/runtime-main.b9f37f5d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
