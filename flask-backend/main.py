1c0f59eca18d28d530202b622f9355b99fbf4ed6
mikelixiang88 <limike@usc.edu>
2020-04-18 00:18:39 -0700

    Add files via upload

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -12,19 +12,58 @@ main = Blueprint('main', __name__)
 def _index(path):
     return flask.render_template("index.html", token="Hello Flask + React")
 
-@app.route('/order')
-def _order():
-    request=[]
-    request.append({'title' : 'avatar', 'rating': '9.1'})
-    print(type(jsonify({"movies:" : request})))
-    return jsonify({"movies:" : request})
+@app.route('/order',methods=['POST','GET'])
+def _main():
+    if request.method != "POST":
+        if Request==True:
+            orders=get_all_orders()
+            print(type(jsonify({"orders:" : orders})))
+            return jsonify({"orders:" : orders})
+        else:
+            helpers=get_all_helpers()
+            print(type(jsonify({"helpers:" : helpers})))
+            return jsonify({"helpers:" : helpers})
+    else:
+        if helper==False:
+            userID=request.get_json()
+            add_to_helper(userID)
+            return 'Done', 201
+        else:
+            userID=request.get_json()
+            delete_from_helper(userID)
+            return 'Done', 201
 
-@app.route('/register', methods=['POST'])
+@app.route('/signup', methods=['POST'])
 def _register():
     register_data = request.get_json()
+        #"[name, email, password]"
+        # "{key : value}"
+        # "{key : array}"
     register(register_data[0], register_data[1], register_data[2])
     print(get_all_users())
     return 'Done', 201
+
+@app.route('/shop', methods=['POST'])
+def _add_to_cart():
+    cart_data = request.get_json()
+        
+    add_to_cart(cart_data[0], cart_data[1], cart_data[2],cart_data[w])
+
+    return 'Done', 201
+
+@app.route('/profile/account')
+def _display():
+    user=get_user(userID)
+    print(type(jsonify({"user:" : user})))
+    return jsonify({"user:" : user})
+
+@app.route('/profile/requests')
+def _order():
+    user_order=get_order_by_user(userID)
+    print(type(jsonify({"user_order:" : user_order})))
+    return jsonify({"user_order:" : user_order})
+
+    
 
 
 
