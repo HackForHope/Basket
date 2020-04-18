005696bd6708aaf12c180c8c1134b33aa3979609
mikelixiang88 <limike@usc.edu>
2020-04-18 03:42:13 -0700

    Add files via upload

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -5,7 +5,6 @@ from sql import *
 
 app = flask.Flask('__main__')
 main = Blueprint('main', __name__)
-mycursor.execute("USE basketdata")
 
 
 @app.route('/', defaults ={'path': ''})
@@ -13,19 +12,13 @@ mycursor.execute("USE basketdata")
 def _index(path):
     return flask.render_template("index.html", token="Hello Flask + React")
 
-@app.route('/order',methods=['POST','GET'])
+@app.route('/home/helpers',methods=['POST','GET'])
 def _main():
-    if request.method != "POST":
-        if Request==True:
-            orders=get_all_orders()
-            print(type(jsonify({"orders:" : orders})))
-            return jsonify({"orders:" : orders})
-        else:
-            helpers=get_all_helpers()
-            print(type(jsonify({"helpers:" : helpers})))
-            return jsonify({"helpers:" : helpers})
-    else:
-        if helper==False:
+    helpers=get_all_helpers()
+    print(type(jsonify({"helpers" : helpers})))
+    return jsonify({"helpers" : helpers})
+    if request.method == "POST":
+        if helper()==False:
             userID=request.get_json()
             add_to_helper(userID)
             return 'Done', 201
@@ -34,6 +27,13 @@ def _main():
             delete_from_helper(userID)
             return 'Done', 201
 
+@app.route('/home/Requests')
+def _main_2():
+     if request.method != "POST":
+         orders=get_all_orders()
+         print(type(jsonify({"orders" : orders})))
+         return jsonify({"orders" : orders})
+    
 @app.route('/signup', methods=['POST'])
 def _register():
     register_data = request.get_json()
@@ -44,46 +44,26 @@ def _register():
     print(get_all_users())
     return 'Done', 201
 
-@app.route('/active-request', methods=['GET'])
-def _activeRequest():
-    request=[]
-    get_active_request()
-    request.append({'title' : 'REQUEST', 'rating': '9.1'})
-    return jsonify({"movies:" : request})
+@app.route('/shop', methods=['POST'])
+def _add_to_cart():
+    cart_data = request.get_json()
+        
+    add_to_cart(cart_data[0], cart_data[1], cart_data[2],cart_data[w])
 
+    return 'Done', 201
 
-@app.route('/active-helper', methods=['GET'])
-def _activeHelper():
-    request=[]
-    get_active_request()
-    print(type(get_active_request))
-    request.append({'title' : 'HELPER', 'rating': '9.1'})
-    return jsonify({"movies:" : request})
+@app.route('/profile/account')
+def _display():
+    user=get_user(userID)
+    print(type(jsonify({"user" : user})))
+    return jsonify({"user" : user})
 
-# @app.route('/place-new-order', methods=['POST','GET'])
-# def _placeNewOrder():
-#     register_data = request.get_json()
-#     register_data = []
-#     register(register_data[0], register_data[1], register_data[2])
-#     print(get_all_users())
-#     return 'Done', 201
-
-
-# @app.route('/shop', methods=['POST'])
-# def _add_to_cart():
-#     cart_data = [request.form["item_id"],request.form["user_id"],request.form["num"],request.form["price"]]
-#     add_to_cart(cart_data[0], cart_data[1], cart_data[2],cart_data[w])
-#     return 'Done', 201
-
-# @app.route('/profile/account')
-# def _display():
-#     user=get_user(userID)
-#     print(type(jsonify({"user:" : user})))
-#     return jsonify({"user:" : user})
-
-
+@app.route('/profile/requests')
+def _order():
+    user_order=get_order_by_user(userID)
+    print(type(jsonify({"user_order" : user_order})))
+    return jsonify({"user_order" : user_order})
 
 # app.register_blueprint(main)
-
 app.run(debug=True)
 con.close()
